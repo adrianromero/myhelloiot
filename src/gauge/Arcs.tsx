e9723cb436cfffb6e36608d4bc7cb317e09d83aa
Adrián Romero <adrianromero@users.noreply.github.com>
2021-07-03 17:48:43 +0200

    Adds arc sections property to circular gauges

---
+++ src/gauge/Arcs.tsx
@@ -0,0 +1,71 @@
+/*
+MYHELLOIOT
+Copyright (C) 2021 Adrián Romero
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import React from "react";
+import { arcpath, padvalue, radians } from "./svgdraw";
+
+export type Arc = {
+  start: number;
+  end: number;
+  r: number;
+  style?: React.CSSProperties;
+};
+
+export type ArcsProps = {
+  arcs: Arc[];
+  min: number;
+  max: number;
+  centerx: number;
+  centery: number;
+  startangle: number;
+  endangle: number;
+};
+
+const Arcs: React.FC<ArcsProps> = ({
+  arcs,
+  min,
+  max,
+  centerx,
+  centery,
+  startangle,
+  endangle,
+}) => (
+  <>
+    {arcs.map((arc) => {
+      const arctotal = endangle - startangle;
+      const arcstart = padvalue(min, max, arctotal)(arc.start) + startangle;
+      const arcend = padvalue(min, max, arctotal)(arc.end) + startangle;
+      return (
+        <path
+          id="arc"
+          d={arcpath({
+            cx: centerx,
+            cy: centery,
+            r: arc.r,
+            start: radians(arcstart),
+            end: radians(arcend),
+            orientation: arcend - arcstart > 180 ? 1 : 0,
+            sweep: 1,
+          })}
+          style={arc.style}
+        />
+      );
+    })}
+  </>
+);
+
+export default Arcs;
