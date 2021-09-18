b339cef7746ebb7d9d8cafe1d893f5d23bc96e01
Adrián Romero <adrianromero@users.noreply.github.com>
2021-09-19 00:56:13 +0200

    Create css files for ModalError

--- src/ModalError.tsx
+++ src/ModalError.tsx
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import React from "react";
 import { Button, Modal } from "antd";
 import { CloseCircleOutlined } from "@ant-design/icons";
+import "./ModalError.css";
 
 const ModalError: React.FC<{
   title: string;
