92989bde86ee1426957d28a1d04a29b515be5f2b
Adrián Romero <adrianromero@users.noreply.github.com>
2022-06-13 00:04:44 +0200

    Updates copyright year

--- src/connection/ContentConnectAbout.tsx
+++ src/connection/ContentConnectAbout.tsx
@@ -307,7 +307,7 @@ const ContentConnectAbout: React.FC<{
           MYHELLOIOT is licensed under the GNU General Public License, Version
           3, 29 June 2007.
         </Paragraph>
-        <Paragraph>Copyright (C) 2021 Adrián Romero.</Paragraph>
+        <Paragraph>Copyright (C) 2021-2022 Adrián Romero.</Paragraph>
       </Typography>
     </>
   );
