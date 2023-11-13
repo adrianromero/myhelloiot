23657bf3430feb04845c64f8f205a83169ad9b7b
Adrián Romero <adrianromero@users.noreply.github.com>
2023-11-13 23:08:35 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/myhelloiot/sw.js', { scope: '/myhelloiot/' })})}
