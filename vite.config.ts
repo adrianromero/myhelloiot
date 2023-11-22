bc7ec7b7426858249f0b1bd57479fe7f066da79b
Adrián Romero <adrianromero@users.noreply.github.com>
2023-11-22 23:27:11 +0100

    Increase PWA maximum file size to cache

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,17 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/myhelloiot/",
-  plugins: [react(), svgr(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: [
+          "**/*.{js,css,html,ico,png,jpg,jpeg,svg,webp,wav,mp3,gltf,bin,eot,ttf,woff,woff2}",
+        ],
+        maximumFileSizeToCacheInBytes: 25097152,
+      },
+    }),
+  ],
 });
